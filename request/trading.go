d7f5cce971bf1275d3678427bc430d080f91e025
Ritus <rituspl@gmail.com>
2023-07-21 11:44:53 +0530

    updated the keys

--- request/trading.go
+++ request/trading.go
@@ -1,13 +1,13 @@
 package request
 
 type Buy struct {
-	Token    string
+	Token    string  `validate:"required"`
 	Amount   float64 `json:"amount" validate:"required"`
 	Currency string  `json:"currency" validate:"required"`
 }
 
 type Sell struct {
-	Token    string
+	Token    string  `validate:"required"`
 	Amount   float64 `json:"amount" validate:"required"`
 	Currency string  `json:"currency" validate:"required"`
 	Price    float64 `json:"price" validate:"required"`
